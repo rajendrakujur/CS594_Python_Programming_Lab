4608dabd07ee372b45715009afce8054868e4275
k_rajendra <31775586+rajendrakujur@users.noreply.github.com>
2021-08-06 19:54:23 +0530

    Create 004.py

---
+++ Lab_Session_01/004.py
@@ -0,0 +1,11 @@
+# Python 3.9.6
+# function to print desired output
+def print_uppercase(string):
+    # inbuilt function to change the string to Uppercase
+    print(string.upper())
+
+
+# execution starts
+string = input('Enter Roman letters : ')
+# function calling
+print_uppercase(string)
