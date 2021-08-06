fbcc81d4eec2c48ee7114e1e75a1a1c3845382fd
k_rajendra <31775586+rajendrakujur@users.noreply.github.com>
2021-08-06 19:52:45 +0530

    Update 001.py

--- Lab_Session_01/001.py
+++ Lab_Session_01/001.py
@@ -1,1 +1,17 @@
 
+# Python 3.9.6
+# function to print pattern, input taken as a constant as per given in problem statement
+def print_pattern(number_of_lines):
+    for i in range(number_of_lines):
+        # loop to print spaces
+        for j in range(i):
+            print(' ', end='')
+        # loop to print asterisks
+        for j in range(1, 2*(number_of_lines-i)):
+            print('*', end='')
+        # to get into the next line extra print() statement required
+        print()
+
+
+# execution starts
+print_pattern(7)
